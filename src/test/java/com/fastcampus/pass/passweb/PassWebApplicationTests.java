5e5ae974ac294cbe901c99aca04cd5471b26248e
kjs92980 <kjs9298@naver.com>
2022-09-23 17:36:13 +0900

    first commit

---
+++ src/test/java/com/fastcampus/pass/passweb/PassWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.pass.passweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
